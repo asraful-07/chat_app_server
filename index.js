25f06cdccbaef96c7a76177502c1ad6e5d455949
asraful-07 <asraful0508@gmail.com>
2025-08-13 12:53:19 +0600

    add clinet deploy url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://talkapp-eac81.web.app"],
     credentials: true,
   })
 );
